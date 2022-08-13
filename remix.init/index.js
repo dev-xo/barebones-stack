aaea83aef5678309f925c003fe56f53a7eee1c23
Dev XO <gitcoinbitcoin@gmail.com>
2022-08-13 23:37:22 +0200

    Updated remix.init | Replaced fly.toml file. v0.1.2

--- remix.init/index.js
+++ remix.init/index.js
@@ -2,8 +2,6 @@
  * Barebones Stack
  * @author @dev-xo: https://github.com/dev-xo
  */
-const { execSync } = require("child_process")
-const crypto = require("crypto")
 const fs = require("fs/promises")
 const path = require("path")
 
@@ -11,10 +9,84 @@ const toml = require("@iarna/toml")
 const sort = require("sort-package-json")
 
 /**
+ * @param {*} string
+ */
+const escapeRegExp = (string) =>
+  // $& means the whole matched string.
+  string.replace(/[.*+?^${}()|[\]\\]/g, "\\$&")
+
+/**
+ * @description
+ * Creates and initiates a newly `.env` file, with provided variables from `.env.example`.
+ */
+const createAndInitEnvFile = async (rootDirectory) => {
+  const ENV_PATH = path.join(rootDirectory, ".env")
+  const EXAMPLE_ENV_PATH = path.join(rootDirectory, ".env.example")
+
+  const exampleEnvFile = await fs.readFile(EXAMPLE_ENV_PATH, "utf-8")
+  await fs.writeFile(ENV_PATH, exampleEnvFile)
+}
+
+/**
+ * @description Replaces default project name for the one provided by `DIR_NAME`.
+ *
+ * Files that are being updated:
+ * - package.json
+ * - fly.toml
+ * - README.md
+ */
+const replaceProjectNameFromFiles = async (rootDirectory, appName) => {
+  const PACKAGE_JSON_PATH = path.join(rootDirectory, "package.json")
+  const FLY_TOML_PATH = path.join(rootDirectory, "fly.toml")
+  const README_PATH = path.join(rootDirectory, "README.md")
+
+  const README_REGEX_HEADER_MATCHER = /#\sBarebones\sStack/gm
+  const APP_NAME_REPLACER_MATCHER = /barebones-stack/gm
+  const README_HEADER_REPLACER = "## " + appName
+
+  // 1. Reads.
+  const [packageJsonFile, tomlFile, readmeFile] = await Promise.all([
+    fs.readFile(PACKAGE_JSON_PATH, "utf-8"),
+    fs.readFile(FLY_TOML_PATH, "utf-8"),
+    fs.readFile(README_PATH, "utf-8"),
+  ])
+
+  // 2. Replaces.
+  const replacedPackageJson =
+    JSON.stringify(
+      sort({ ...JSON.parse(packageJsonFile), name: appName }),
+      null,
+      2
+    ) + "\n"
+
+  const replacedToml = toml.parse(tomlFile)
+  replacedToml.app = replacedToml.app.replace(
+    APP_NAME_REPLACER_MATCHER,
+    appName
+  )
+
+  const replacedReadmeHeader = readmeFile.replace(
+    README_REGEX_HEADER_MATCHER,
+    README_HEADER_REPLACER
+  )
+  const replacedReadmeHeaderAndText = replacedReadmeHeader.replace(
+    APP_NAME_REPLACER_MATCHER,
+    appName
+  )
+
+  // 3. Writes.
+  await Promise.all([
+    fs.writeFile(PACKAGE_JSON_PATH, replacedPackageJson),
+    fs.writeFile(FLY_TOML_PATH, toml.stringify(replacedToml)),
+    fs.writeFile(README_PATH, replacedReadmeHeaderAndText),
+  ])
+}
+
+/**
  * @description
  * Runs after the project has been generated and dependencies have been installed.
  */
-async function main({ rootDirectory, packageManager, isTypeScript }) {
+const main = async ({ rootDirectory, packageManager, isTypeScript }) => {
   const DIR_NAME = path.basename(rootDirectory)
   const APP_NAME = DIR_NAME.replace(/[^a-zA-Z0-9-_]/g, "-")
 
@@ -44,73 +116,6 @@ async function main({ rootDirectory, packageManager, isTypeScript }) {
 Start development with \`npm run dev\`
  `.trim()
   )
-}
-
-function escapeRegExp(string) {
-  // $& means the whole matched string.
-  return string.replace(/[.*+?^${}()|[\]\\]/g, "\\$&")
-}
-
-function getRandomString(length) {
-  return crypto.randomBytes(length).toString("hex")
-}
-
-/**
- * @description
- * Creates and initiates a newly `.env` file, with provided variables from `.env.example`.
- */
-const createAndInitEnvFile = async (rootDirectory) => {
-  const ENV_PATH = path.join(rootDirectory, ".env")
-  const EXAMPLE_ENV_PATH = path.join(rootDirectory, ".env.example")
-
-  const exampleEnvFile = await fs.readFile(EXAMPLE_ENV_PATH, "utf-8")
-  await fs.writeFile(ENV_PATH, exampleEnvFile)
-}
-
-/**
- * @description Replaces default project name for the one provided by `DIR_NAME`.
- *
- * Files that are being updated:
- * - package.json
- * - README.md
- *
- * // TODO: Fly.toml
- */
-async function replaceProjectNameFromFiles(rootDirectory, appName) {
-  const PACKAGE_JSON_PATH = path.join(rootDirectory, "package.json")
-  const README_PATH = path.join(rootDirectory, "README.md")
-
-  const README_REGEX_HEADER_MATCHER = /##\sBarebones\sStack/gm
-  const README_REGEX_TEXT_MATCHER = /barebones-stack/gm
-  const README_HEADER_APP_NAME = "## " + appName
-
-  // 1. Reads.
-  const [packageJsonFile, readmeFile] = await Promise.all([
-    fs.readFile(PACKAGE_JSON_PATH, "utf-8"),
-    fs.readFile(README_PATH, "utf-8"),
-  ])
-
-  // 2. Replaces.
-  const replacedPackageJsonFile =
-    JSON.stringify(
-      sort({ ...JSON.parse(packageJsonFile), name: appName }),
-      null,
-      2
-    ) + "\n"
-  const replacedHeaderReadmeFile = readmeFile.replace(
-    README_REGEX_HEADER_MATCHER,
-    README_HEADER_APP_NAME
-  )
-  const replacedHeaderAndTextReadmeFile = replacedHeaderReadmeFile.replace(
-    README_REGEX_TEXT_MATCHER,
-    appName
-  )
-
-  // 3. Writes.
-  await Promise.all([
-    fs.writeFile(PACKAGE_JSON_PATH, replacedPackageJsonFile),
-    fs.writeFile(README_PATH, replacedHeaderAndTextReadmeFile),
-  ])
 }
 
 module.exports = main

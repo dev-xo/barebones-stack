fa755d26487e11659bcecfaf55a7d7378c8ad2a1
Dev XO <gitcoinbitcoin@gmail.com>
2022-08-14 15:01:43 +0200

    Redo database change. This will be acomplished on dev branch.

--- prisma/migrations/20220814124804_init/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "Welcome" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "message" TEXT,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" DATETIME NOT NULL
-);

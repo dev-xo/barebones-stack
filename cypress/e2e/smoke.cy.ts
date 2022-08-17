3be10f23a7741d2287b7f933d0449b43ccf4a603
Dev XO <code.devxo@gmail.com>
2022-08-17 19:01:59 +0200

    Update smoke.cy.ts

--- cypress/e2e/smoke.cy.ts
+++ cypress/e2e/smoke.cy.ts
@@ -1,9 +1,16 @@
 /**
  * Example E2E Test.
+ *
+ * Besides the tests passes correctly, Cypress is giving us the following (uncaught exception) error:
+ * Error: Hydration failed because the initial UI does not match what was rendered on the server.
+ *
+ * The team is working on it's fix already.
+ * Check cypress/support/e2e for a detailed explanation.
+ *
  */
 describe("Smoke test!.", () => {
-  it("Should display Barebones welcome text.", () => {
+  it("Should display template name text.", () => {
     cy.visit("/");
-    cy.findByText("Remix Stack");
+    cy.findByText("Barebones Stack");
   });
 });

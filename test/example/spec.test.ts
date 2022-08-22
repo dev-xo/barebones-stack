f0f589f671d51ad2345494e2fe91ae0f75814967
Dev XO <code.devxo@gmail.com>
2022-08-22 20:09:22 +0200

    style: format repository

--- test/example/spec.test.ts
+++ test/example/spec.test.ts
@@ -1,8 +1,8 @@
-import { describe, expect, it } from "vitest"
+import { describe, expect, it } from "vitest";
 
 describe("Example test.", () => {
   it("It should pass successfully.", () => {
-    const hasPassed = true
-    expect(hasPassed).toBeTruthy()
-  })
-})
+    const hasPassed = true;
+    expect(hasPassed).toBeTruthy();
+  });
+});

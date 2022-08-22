46642abfa16e9d41a233d8ea3c0985f322194293
Dev XO <code.devxo@gmail.com>
2022-08-23 00:05:00 +0200

    Docs: update comment

--- app/routes/healthcheck.tsx
+++ app/routes/healthcheck.tsx
@@ -1,9 +1,15 @@
-/**
- * Learn more: https://fly.io/docs/reference/configuration/#services-http_checks
- */
 import type { LoaderArgs } from "@remix-run/node"
 import { prisma } from "~/utils/db.server"
 
+/**
+ * Remix - Loader.
+ * @protected Template code.
+ *
+ * If we can connect to database to make a simple query
+ * and a HEAD request to ourselves, then we're good.
+ *
+ * Learn more: https://fly.io/docs/reference/configuration/#services-http_checks
+ */
 export async function loader({ request }: LoaderArgs) {
   const host =
     request.headers.get("X-Forwarded-Host") ?? request.headers.get("host")

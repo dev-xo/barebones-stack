a47ada6c4f13987fa97181dbac3fb2934bf77d6b
Dev XO <code.devxo@gmail.com>
2022-08-25 21:23:04 +0200

    Chore: bring files in line with templates
    Getting up with the latest updates from Indie Stack.

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -21,26 +21,31 @@ export default function handleRequest(
     let didError = false
 
     const { pipe, abort } = renderToPipeableStream(
-      <RemixServer context={remixContext} url={request.url} />,
+      <RemixServer
+        context={remixContext}
+        url={request.url}
+      />,
       {
-        [callbackName]() {
-          let body = new PassThrough()
+        [callbackName]: () => {
+          const body = new PassThrough()
 
           responseHeaders.set("Content-Type", "text/html")
 
           resolve(
             new Response(body, {
-              status: didError ? 500 : responseStatusCode,
               headers: responseHeaders,
+              status: didError ? 500 : responseStatusCode,
             })
           )
+
           pipe(body)
         },
-        onShellError(err: unknown) {
+        onShellError: (err: unknown) => {
           reject(err)
         },
-        onError(error: unknown) {
+        onError: (error: unknown) => {
           didError = true
+
           console.error(error)
         },
       }

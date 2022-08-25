e6a5576171d702eea16cd0c40deb78b8090c32b0
Dev XO <code.devxo@gmail.com>
2022-08-25 21:02:00 +0200

    Fix: typo on `db.server` utils

--- app/utils/db.server.ts
+++ app/utils/db.server.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from "@prisma/client"
 
-let db: PrismaClient
+let prisma: PrismaClient
 
 declare global {
   var __db__: PrismaClient
@@ -9,11 +9,11 @@ declare global {
 // In development we don't want to restart the server with every change,
 // and we want to make sure we don't create a new connection to the DB with every change either.
 // In production we'll have a single connection to the DB.
-if (process.env.NODE_ENV === "production") db = new PrismaClient()
+if (process.env.NODE_ENV === "production") prisma = new PrismaClient()
 else {
   if (!global.__db__) global.__db__ = new PrismaClient()
-  db = global.__db__
-  db.$connect()
+  prisma = global.__db__
+  prisma.$connect()
 }
 
-export { db }
+export { prisma }

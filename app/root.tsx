238e145bfee8e507a83b43c09bc5dabd1232eadf
Dev XO <code.devxo@gmail.com>
2022-08-26 21:33:17 +0200

    Chore: update `Meta` description

--- app/root.tsx
+++ app/root.tsx
@@ -14,11 +14,32 @@ export const links: LinksFunction = () => {
   return [{ rel: "stylesheet", href: tailwindStylesheetUrl }]
 }
 
-export const meta: MetaFunction = () => ({
-  charset: "utf-8",
-  title: "New Remix App",
-  viewport: "width=device-width,initial-scale=1",
-})
+export const meta: MetaFunction = () => {
+  return {
+    charset: "utf-8",
+    title: "Welcome to Remix Barebones!",
+    description:
+      "A solid create-remix app, that applies best practices into a clean, batteries included template. SQLite version. Deploys to Fly.io",
+    keywords:
+      "remix,create-remix,remix-stack,typescript,sqlite,prisma,tailwindcss,fly.io",
+    "og:title": "Remix Barebones Stack",
+    "og:type": "website",
+    "og:url": "https://barebones-stack.fly.dev",
+    "og:image":
+      "https://raw.githubusercontent.com/dev-xo/barebones-stack/main/app/assets/images/thumbnail.png",
+    "og:card": "summary_large_image",
+    "og:creator": "@DanielKanem",
+    "og:site": "https://barebones-stack.fly.dev",
+    "og:description":
+      "A solid create-remix app, that applies best practices into a clean, batteries included template. SQLite version. Deploys to Fly.io",
+    "twitter:image": "https://barebones-stack.fly.dev",
+    "twitter:card": "summary_large_image",
+    "twitter:creator": "@DanielKanem",
+    "twitter:title": "Remix Barebones Stack",
+    "twitter:description":
+      "A solid create-remix app, that applies best practices into a clean, batteries included template. SQLite version. Deploys to Fly.io",
+  }
+}
 
 export default function App() {
   return (

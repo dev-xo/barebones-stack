02c70d5056c3484ade782b1325f46c500d0f6b06
Dev XO <code.devxo@gmail.com>
2022-10-23 00:17:54 +0200

    Feat: add playwright testing
    Cypress is no longer available as E2E testing.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,13 @@
 /**
  * @type {import('@types/eslint').Linter.BaseConfig}
  */
-module.exports = {
+ module.exports = {
 	extends: [
 		'@remix-run/eslint-config',
 		'@remix-run/eslint-config/node',
 		'@remix-run/eslint-config/jest-testing-library',
 		'prettier',
 	],
-	env: {
-		'cypress/globals': true,
-	},
-	plugins: ['cypress'],
 
 	// We're using vitest which has a very similar API to jest,
 	// (so the linting plugins work nicely), but we have to explicitly set the jest version.
